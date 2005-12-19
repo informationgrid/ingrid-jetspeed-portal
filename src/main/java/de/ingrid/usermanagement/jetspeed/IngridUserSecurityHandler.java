518aeacfb779f2a39d62ce2904d0642fde83b35b
ms <ms@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-12-19 13:12:30 +0000

    o implement UserSecurityHandler o remove unnecessary classes

--- src/main/java/de/ingrid/usermanagement/jetspeed/IngridUserSecurityHandler.java
+++ src/main/java/de/ingrid/usermanagement/jetspeed/IngridUserSecurityHandler.java
@@ -1,68 +1,169 @@
 /*
  * Copyright (c) 1997-2005 by media style GmbH
- * 
- * $Source: $
  */
 
 package de.ingrid.usermanagement.jetspeed;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.jetspeed.security.SecurityException;
 import org.apache.jetspeed.security.UserPrincipal;
+import org.apache.jetspeed.security.impl.UserPrincipalImpl;
 import org.apache.jetspeed.security.spi.UserSecurityHandler;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import de.ingrid.usermanagement.HibernateUtil;
+import de.ingrid.usermanagement.UserManagement;
 
 /**
- * TODO: implement this
  */
 public class IngridUserSecurityHandler implements UserSecurityHandler {
+
+    private final Log fLogger = LogFactory.getLog(this.getClass());
+
+    private Session fSession;
+
+    /**
+     */
+    public IngridUserSecurityHandler() {
+        this.fSession = HibernateUtil.currentSession();
+    }
 
     /**
      * @see org.apache.jetspeed.security.spi.UserSecurityHandler#isUserPrincipal(java.lang.String)
      */
-    public boolean isUserPrincipal(String arg0) {
-        // TODO Auto-generated method stub
-        return false;
+    public boolean isUserPrincipal(String userName) {
+        verifyUserName(userName);
+
+        return getUserPrincipal(userName) != null;
     }
 
     /**
      * @see org.apache.jetspeed.security.spi.UserSecurityHandler#getUserPrincipal(java.lang.String)
      */
-    public Principal getUserPrincipal(String arg0) {
-        // TODO Auto-generated method stub
-        return null;
+    public Principal getUserPrincipal(String userName) {
+        Principal result = null;
+
+        verifyUserName(userName);
+
+        Transaction tx = this.fSession.beginTransaction();
+        List authList = this.fSession.createQuery("from UserManagement").list();
+        try {
+            UserManagement um = (UserManagement) authList.get(0);
+
+            if (um.userExists(userName)) {
+                result = new UserPrincipalImpl(userName);
+            }
+        } catch (IndexOutOfBoundsException e) {
+            this.fLogger.error("An hibernate error has occurred for user name:" + userName);
+        } finally {
+            tx.commit();
+        }
+
+        return result;
     }
 
     /**
      * @see org.apache.jetspeed.security.spi.UserSecurityHandler#getUserPrincipals(java.lang.String)
      */
-    public List getUserPrincipals(String arg0) {
-        // TODO Auto-generated method stub
-        return null;
+    public List getUserPrincipals(String filter) {
+        List result = new ArrayList();
+        
+        Transaction tx = this.fSession.beginTransaction();
+        List authList = this.fSession.createQuery("from UserManagement").list();
+        try {
+            UserManagement um = (UserManagement) authList.get(0);
+            
+            String[] userNames = um.find(filter);
+            for (int i = 0; i < userNames.length; i++) {
+                result.add(new UserPrincipalImpl(userNames[i]));
+            }
+        } catch (IndexOutOfBoundsException e) {
+            this.fLogger.error("An hibernate error has occurred for filter:" + filter);
+        } finally {
+            tx.commit();
+        }
+        
+        return result;
     }
 
     /**
      * @see org.apache.jetspeed.security.spi.UserSecurityHandler#addUserPrincipal(org.apache.jetspeed.security.UserPrincipal)
      */
-    public void addUserPrincipal(UserPrincipal arg0) throws SecurityException {
-        // TODO Auto-generated method stub
+    public void addUserPrincipal(UserPrincipal userPrincipal) throws SecurityException {
+        verifyUserPrincipal(userPrincipal);
 
+        String userName = userPrincipal.getName();
+        if (isUserPrincipal(userName)) {
+            throw new SecurityException(SecurityException.USER_ALREADY_EXISTS.create(userName));
+        }
+
+        Transaction tx = this.fSession.beginTransaction();
+        List authList = this.fSession.createQuery("from UserManagement").list();
+        try {
+            UserManagement um = (UserManagement) authList.get(0);
+
+            // FIXME: Add the user here but with its name as the password.
+            um.addUser(userName, userName);
+            this.fSession.update(um);
+        } catch (IndexOutOfBoundsException e) {
+            this.fLogger.error("An hibernate error has occurred for user name:" + userName);
+        } finally {
+            tx.commit();
+        }
+    }
+
+    private void verifyUserPrincipal(UserPrincipal userPrincipal) {
+        if (userPrincipal == null) {
+            throw new IllegalArgumentException("The UserPrincipal cannot be null or empty.");
+        }
     }
 
     /**
      * @see org.apache.jetspeed.security.spi.UserSecurityHandler#updateUserPrincipal(org.apache.jetspeed.security.UserPrincipal)
      */
-    public void updateUserPrincipal(UserPrincipal arg0) throws SecurityException {
-        // TODO Auto-generated method stub
-
+    public void updateUserPrincipal(UserPrincipal userPrincipal) throws SecurityException {
+        verifyUserPrincipal(userPrincipal);
+        String userName = userPrincipal.getName();
+        if (!isUserPrincipal(userName)) {
+            addUserPrincipal(userPrincipal);
+        }
     }
 
     /**
      * @see org.apache.jetspeed.security.spi.UserSecurityHandler#removeUserPrincipal(org.apache.jetspeed.security.UserPrincipal)
      */
-    public void removeUserPrincipal(UserPrincipal arg0) throws SecurityException {
-        // TODO Auto-generated method stub
+    public void removeUserPrincipal(UserPrincipal userPrincipal) throws SecurityException {
+        verifyUserPrincipal(userPrincipal);
+
+        String userName = userPrincipal.getName();
+
+        Transaction tx = this.fSession.beginTransaction();
+        List authList = this.fSession.createQuery("from UserManagement").list();
+        try {
+            UserManagement um = (UserManagement) authList.get(0);
+
+            um.removeUser(userName);
+            this.fSession.update(um);
+        } catch (IndexOutOfBoundsException e) {
+            this.fLogger.error(e);
+            throw new SecurityException(SecurityException.UNEXPECTED
+                    .create("An hibernate error has occurred for user name:" + userName));
+        } finally {
+            tx.commit();
+        }
+    }
+
+    private void verifyUserName(String userName) {
+        if (StringUtils.isEmpty(userName)) {
+            throw new IllegalArgumentException("The user name cannot be null or empty.");
+        }
     }
 
 }
