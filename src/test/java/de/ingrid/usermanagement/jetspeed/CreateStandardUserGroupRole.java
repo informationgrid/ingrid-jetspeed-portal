3e5f212ec8f9789dad0ca5bef6b53abe28aee8b5
ms <ms@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-01-12 13:10:01 +0000

    o add a sleep after closing the hibernate session so the data   is written to the db

--- src/test/java/de/ingrid/usermanagement/jetspeed/CreateStandardUserGroupRole.java
+++ src/test/java/de/ingrid/usermanagement/jetspeed/CreateStandardUserGroupRole.java
@@ -13,16 +13,20 @@ import de.ingrid.usermanagement.UserManagement;
 public class CreateStandardUserGroupRole {
 
     /**
-     * Creates the standard user. 
+     * Creates the standard user.
+     * 
      * @param args
+     * @throws InterruptedException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         UserManagement um = new UserManagement();
         um.addRole("admin");
         um.addGroup("admin");
         um.addUser("admin", "admin");
-        
+
         um.addUserToGroup("admin", "admin", "admin");
         HibernateManager.getInstance().closeSession();
+
+        Thread.sleep(30000);
     }
 }
